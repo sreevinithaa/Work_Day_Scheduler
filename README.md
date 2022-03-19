b0a0b665ed2494af1fbfc234339be360d0c0a5ec
Vinitha Gowtheepan <sreevinithaa@gmail.com>
2022-03-19 16:32:44 +0800

    Update comment and screenshot

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Work_Day_Scheduler
-Simple calendar application that allows a user to save events for each hour of the day.This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery
+
+## Description
+simple calendar application that allows a user to save events for each hour of the day
+
+## Table of content
+Quiz,initial and view high score
+
+## Image of site
+![A user clicks on slots on the color-coded calendar and edits the events.](https://sreevinithaa.github.io/Work_Day_Scheduler/assets/img/screenshot2.png)
+
+## Repository
+
+You can get the code in the following link. https://github.com/sreevinithaa/Work_Day_Scheduler.git
+
+## Production link
+
+You can view my portfolio through this live link https://sreevinithaa.github.io/Work_Day_Scheduler/
+
+
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,41 @@
 var currentDay = $("#currentDay");
 var dayplannertable = $("#dayplaner");
 var today = moment();
-const starthour =12;
+const starthour = 12;
 const endhour = 19;
 var currenthour = 0;
 
+
+//Set current date and update every 10 min if hour increase then table will get re-load again.
 function CurrentDayUpdate() {
   currenthour = today.format("HH");
   loadTable();
+  currentDay.text(today.format("MMM Do, YYYY"));
   setInterval(function () {
     currentDay.text(today.format("MMM Do, YYYY"));
     if (today.format("HH") != currenthour) {
       currenthour = today.format("HH");
       loadTable();
     }
-  }, 1000);
+  }, 600000);
 
   return;
 }
+
+//creating a row every hour
 function CreatRow(i) {
   var calendertime = moment(`${i}:00`, "HH:mm");
+
+  //retrieve data from local storage
   var plan = GetData(calendertime.format("h:mm a"));
 
   var tr = $("<tr>");
   var td1 = $("<td>").addClass("column-1").text(calendertime.format("h:mm a"));
 
   var td2 = $("<td>").addClass("column-2").text(plan);
-  console.log("i= " + i);
-  console.log("currenthour= " + currenthour);
+ 
+
+  //Check the row is future,past or present.Only can add plan through present and future
   if (i == currenthour) {
     td2.addClass("present");
     td2.on("click", ColumnClick);
@@ -42,17 +50,21 @@ function CreatRow(i) {
   var button = $("<button>");
   button.addClass("btnsave").attr("data-time", calendertime.format("h:mm a"));
   button.on("click", SaveButtonClick);
-var icon=$("<i>").attr("class","fas fa-save fa-lg")
-  //button.text("Save");
+
+  //adding save icon to the button
+  var icon = $("<i>").attr("class", "fas fa-save fa-lg");
+  
   button.append(icon);
   td3.append(button);
 
   button.attr("disabled", "disabled");
   td3.addClass("column-3");
   tr.append(td1, td2, td3);
-// <i class="fa fa-folder"></i>
+  
   return tr;
 }
+
+//Generate table
 function loadTable() {
   $("#dayplaner tr").remove();
 
@@ -62,6 +74,8 @@ function loadTable() {
   }
   return;
 }
+
+//this function will get call when column is clicked to add plan
 function ColumnClick(event) {
   var nexttd = $(event.target).siblings(".column-3");
   var tds = $(event.target).children();
@@ -75,11 +89,14 @@ function ColumnClick(event) {
     input.focus();
   }
 }
+
+//when text change is triggered this method will get called
 function InputTextChange(event) {
   var plan = $(event.target).val();
   var datetime = $(event.target).parent().siblings(".column-1").text();
   SaveData(datetime, plan);
 }
+//this method will get call when save button click
 function SaveButtonClick(event) {
   var button = $(event.target);
   var datetime = button.attr("data-time");
@@ -92,11 +109,15 @@ function SaveButtonClick(event) {
 
   return;
 }
+
+//helper method to check empty
 function isEmpty(value) {
   return value == undefined || value == null || value.length === 0;
 }
+
+//Save data into local storage
 function SaveData(datetime, description) {
-  console.log("datett : " + datetime);
+ 
   var plan = localStorage.getItem(datetime);
 
   if (isEmpty(plan)) {
@@ -107,6 +128,8 @@ function SaveData(datetime, description) {
   loadTable();
   return;
 }
+
+//get data from local storage
 function GetData(datetime) {
   var plan = localStorage.getItem(datetime);
   return plan;
